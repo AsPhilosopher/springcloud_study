0f0f25856a7611824eff91a188edf26f944625f3
Lincon <1003349954@qq.com>
2018-07-11 23:59:23 +0800

    高可用注册中心

--- eureka_server1/src/main/java/com/study/springcloud/eureka_server1/EurekaServerApplication.java
+++ eureka_server1/src/main/java/com/study/springcloud/eureka_server1/EurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer1Application {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer1Application.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- eureka_server2/src/main/java/com/study/springcloud/eureka_server2/EurekaServerApplication.java
+++ eureka_server2/src/main/java/com/study/springcloud/eureka_server2/EurekaServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer2Application {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServer2Application.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 }
